bf7fe6b18cb4e5e2fb02ec2033764a6286efd878
Luis Eduardo Sotoj <luissotoj@gmail.com>
2020-06-13 22:50:15 -0600

    Añadiendo rutas a server.js para manejar los get post delet

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express');
 // import express from 'express'; --> es la sintaxis emc6 es lo mismo
-
+const router = express.Router();
 var app = express();
 
-app.use('/', function(req, res){
-    res.send('Hola');
+app.use(router);
+
+router.get('/messaje', function(req, res){
+    res.send('Lista de mensajes');
 });
+
+router.post('/messaje', function(req, res){
+    res.send('Mensaje añadido');
+});
+
+// app.use('/', function(req, res){
+//     res.send('Hola');
+// });
 
 app.listen(3000);
 console.log('la aplicacion está escuchando en http://localhost:3000');
