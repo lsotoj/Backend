6ba59585fe8a3232a0574ff25951d95976c8a878
Luis Eduardo Sotoj <luissotoj@gmail.com>
2020-06-17 04:47:07 -0600

    Respondiendo desde el servidor agregando datos en el header, tambien respondiendo con status por ejemplo 201, devolviendo arrays como respuesta

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 const express = require('express');
-// import express from 'express'; --> es la sintaxis emc6 es lo mismo
+const bodyParser = require('body-parser');
+//import express from 'express'; --> es la sintaxis emc6 es lo mismo
 const router = express.Router();
-var app = express();
 
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(router);
 
+
 router.get('/messaje', function(req, res){
+    console.log(req.headers);
+    res.header({
+        "custom-header": "Nuestro valor personalizado"
+    });
     res.send('Lista de mensajes');
 });
 
 router.post('/messaje', function(req, res){
-    res.send('Mensaje añadido');
+    console.log(req.body);
+    console.log(req.query);
+    res.status(201).send([{Error: '', body: 'Creado correctamente'}]);
 });
 
 // app.use('/', function(req, res){
