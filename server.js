0d33ade72754fbc6596b4ded61c38d861a508f6a
Luis Eduardo Sotoj <luissotoj@gmail.com>
2020-06-17 06:09:17 -0600

    Homologando nuestras respuestas del servidior mediante el archivo response.js en el cual administramos las respuestas de SUCCESS y ERROR

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-//import express from 'express'; --> es la sintaxis emc6 es lo mismo
+const response = require('./network/response');
+//const { response } = require('express');
 const router = express.Router();
 
 var app = express();
@@ -14,18 +15,18 @@ router.get('/messaje', function(req, res){
     res.header({
         "custom-header": "Nuestro valor personalizado"
     });
-    res.send('Lista de mensajes');
+    response.success(req, res,'lista de mensajes');
 });
 
 router.post('/messaje', function(req, res){
-    console.log(req.body);
     console.log(req.query);
-    res.status(201).send([{Error: '', body: 'Creado correctamente'}]);
+    if(req.query.error == 'ok') {
+        response.error(req, res, 'Error simulado',401);
+    } else {
+        response.success(req, res, 'Creado correctamente', 201);
+    }
 });
 
-// app.use('/', function(req, res){
-//     res.send('Hola');
-// });
 
 app.listen(3000);
 console.log('la aplicacion está escuchando en http://localhost:3000');
